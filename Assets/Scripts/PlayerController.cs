b9cd8782c0e24d7a071b6a8ce8b05802891ccba1
Max Bronstring <max.brons@hotmail.com>
2020-01-21 17:52:32 +0100

    PlayerMovement changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,25 +5,47 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Camera cam;
-    [SerializeField] private float rotationSpeed = 50;
-    private float movementSpeed = 30;
+    private float rotationSpeed = 50;
+    private float movementSpeed = 20;
     private int health = 200;
     private bool isCrouching = false;
     private bool isJumping = false;
     private Vector2 mouseLook;
     private Vector2 smoothV;
+    private Rigidbody rb;
+    private float xRot = 0;
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        Vector2 md = new Vector2(-Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X"));
-        mouseLook.x = Mathf.Clamp(mouseLook.x, -70f, 70f);
-        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+        /*Vector2 md = new Vector2(-Input.GetAxisRaw(ConstClass.MOUSEY), Input.GetAxisRaw(ConstClass.MOUSEX));
+        mouseLook.x = Mathf.Clamp(mouseLook.x, -45f, 45f);
         mouseLook += md;
         cam.transform.localRotation = Quaternion.Euler(mouseLook);
+        Quaternion rot = cam.transform.localRotation;*/
+        float mouseX = Input.GetAxisRaw(ConstClass.MOUSEX) * movementSpeed * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw(ConstClass.MOUSEY) * movementSpeed * Time.deltaTime;
+        xRot += mouseY;
+        xRot = Mathf.Clamp(xRot, -90, 90);
+
+        transform.localRotation = cam.transform.rotation;
+        cam.transform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void FixedUpdate()
+    {
+        float hAxis = Input.GetAxis(ConstClass.HORIZONTAL);
+        float vAxis = Input.GetAxis(ConstClass.VERTICAL);
+
+        Vector3 hMove = hAxis * transform.right;
+        Vector3 vMove = vAxis * transform.forward;
+        Vector3 Movement = transform.position + (hMove + vMove).normalized * movementSpeed * Time.fixedDeltaTime;
+
+        rb.MovePosition(Movement);
     }
 }
