0e8b0f0d9d17e47b3717445ee82eb1c416d8b90d
Max Bronstring <max.brons@hotmail.com>
2020-01-22 21:16:41 +0100

    Gun model and animations added

--- Assets/Scripts/Shotgun.cs
+++ Assets/Scripts/Shotgun.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Shotgun : Weapons
 {
     private float shoot_Timer = 0;
-    private int bullets_Magazine = 10;
-    private int bullets_Reserve;
+    private int bullets_Magazine = 6;
+    private int bullets_Reserve = 30;
 
     private void Awake()
     {
         this.fpsCamera = Camera.main;
-        //this.animator = GetComponent<Animator>();
+        this.animator = GetComponent<Animator>();
         this.range = ConstClass.SHOTGUN_RANGE;
         this.damage = ConstClass.SHOTGUN_DAMAGE;
         this.attackSpeed = ConstClass.SHOTGUN_ATACKSPEED;
@@ -24,7 +24,7 @@ public class Shotgun : Weapons
         shoot_Timer += Time.deltaTime;
         if (Input.GetMouseButtonDown(0) && shoot_Timer >= attackSpeed && bullets_Magazine > 0)
         {
-            Shoot();
+            StartCoroutine(Shoot());
             shoot_Timer = 0;
         }
 
@@ -32,33 +32,52 @@ public class Shotgun : Weapons
             StartCoroutine(Reload());
     }
 
+    public void LoadBullet()
+    {
+        bullets_Magazine++;
+        bullets_Reserve--;
+    }
+
     private IEnumerator Reload()
     {
-        /*if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !animator.IsInTransition(0))
+        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !animator.IsInTransition(0) && bullets_Magazine < 6)
         {
             //Start reload animation
+            animator.SetBool("Reloading", true);
+            animator.SetTrigger("Reload");
 
-            while (animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1)
+            while (animator.GetBool("Reloading") == true)
+            {
+                if(bullets_Magazine >= 6)
+                    animator.SetBool("Reloading", false);
+
                 yield return null;
-
-            bullets_Reserve--;
-        }*/
-        bullets_Magazine++;
+            }  
+        }
         yield return null;
     }
 
-    private void Shoot()
+    private IEnumerator Shoot()
     {
-        //Start shotgun shoot animation
-        //Start shotgun muzzleflash animation/particle system
-        bullets_Magazine--;
-
-        RaycastHit hit;
-        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        if (bullets_Magazine > 0)
         {
-            Vector3 forward = fpsCamera.transform.TransformDirection(Vector3.forward) * 10;
-            if (hit.transform.gameObject.tag == ConstClass.TARGET_NAME)
-                hit.transform.gameObject.GetComponent<Target>().Hit();
+            while (animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1)
+                yield return null;
+
+            //Start shotgun shoot animation
+            animator.SetTrigger("Shoot");
+
+            animator.SetBool("Reloading", false);
+            bullets_Magazine--;
+
+            RaycastHit hit;
+            if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+            {
+                Vector3 forward = fpsCamera.transform.TransformDirection(Vector3.forward) * 10;
+                if (hit.transform.gameObject.tag == ConstClass.TARGET_NAME)
+                    hit.transform.gameObject.GetComponent<Target>().Hit();
+            }
         }
+        yield return null;
     }
 }
