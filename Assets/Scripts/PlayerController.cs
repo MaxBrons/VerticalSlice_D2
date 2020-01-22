1d714d068d14faa7207033103526d2b3bbf3ae1f
Max Bronstring <max.brons@hotmail.com>
2020-01-22 16:17:22 +0100

    FPS Controller Done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Camera cam;
+    private Rigidbody rb;
     private float rotationSpeed = 50;
     private float movementSpeed = 5;
     private int health = 200;
@@ -14,27 +15,26 @@ public class PlayerController : MonoBehaviour
     private Vector2 smoothV;
     private float mouseX = 0;
     private float mouseY = 0;
+    private bool jumping = false;
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        /*Vector2 md = new Vector2(-Input.GetAxisRaw(ConstClass.MOUSEY), Input.GetAxisRaw(ConstClass.MOUSEX));
-        mouseLook.x = Mathf.Clamp(mouseLook.x, -45f, 45f);
-        mouseLook += md;
-        cam.transform.localRotation = Quaternion.Euler(mouseLook);
-        Quaternion rot = cam.transform.localRotation;*/
         mouseY += Input.GetAxisRaw(ConstClass.MOUSEX) * rotationSpeed * Time.deltaTime;
         mouseX -= Input.GetAxisRaw(ConstClass.MOUSEY) * rotationSpeed * Time.deltaTime;
 
         mouseX = Mathf.Clamp(mouseX, -90, 90);
         transform.rotation = Quaternion.Euler(0, mouseY, 0);
         cam.transform.rotation = Quaternion.Euler(mouseX, mouseY, 0);
-    }
 
+        if (Input.GetKeyDown(KeyCode.Space) && jumping == false) 
+            rb.AddForce(new Vector3(0, 500, 0));
+    }
     private void FixedUpdate()
     {
         float hAxis = Input.GetAxis(ConstClass.HORIZONTAL);
@@ -45,5 +45,16 @@ public class PlayerController : MonoBehaviour
         Vector3 Movement = (hMove + vMove) * movementSpeed * Time.deltaTime;
 
         transform.position += Movement;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        jumping = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == ConstClass.GROUND_TAG)
+            jumping = false;
     }
 }
